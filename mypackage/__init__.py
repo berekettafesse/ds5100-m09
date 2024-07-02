2b95d979a54540f070c5db239e3c3fc39ba51ba5
Bereket Tafesse <uqs3dq@virginia.edu>
2024-07-02 11:13:58 -0400

    package is working

---
+++ mypackage/__init__.py
@@ -0,0 +1,1 @@
+print("Thank you for importing my package!")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mypackage',
+    version='1.0',
+    description= 'Simple participation package',
+    url='https://github.com/berekettafesse/ds5100-m09',
+    author='Bereket Tafesse',
+    author_email='uqs3dq@virginia.edu',
+    license='MIT',
+    packages=['mypackage'],
+)
